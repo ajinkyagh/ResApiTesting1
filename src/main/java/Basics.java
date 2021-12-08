b91f4596a7655460e0a25c13d81dfb35167cfd6f
ajinkyagh <ajinkyagh@gmail.com>
2021-12-08 11:05:10 +0530

    Updates existing place , retreives updated place and also improves readabilty of code

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -1,15 +1,18 @@
+import files.ReUsableMethods;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
+import org.testng.Assert;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.config.EncoderConfig.encoderConfig;
-import  static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.equalTo;
 
 public class Basics {
-    public static void main(String[] args){
-        RestAssured.baseURI="https://rahulshettyacademy.com";
-        String response=given().config(RestAssured.config().encoderConfig(encoderConfig().encodeContentTypeAs("application/jason", ContentType.TEXT))).log().all().queryParam("key",": key =qaclick123").header("Content-Type","application/jason")
+    public static void main(String[] args) {
+        RestAssured.baseURI = "https://rahulshettyacademy.com";
+//        create a place
+        String response = given().config(RestAssured.config().encoderConfig(encoderConfig().encodeContentTypeAs("application/jason", ContentType.TEXT))).log().all().queryParam("key", ": key =qaclick123").header("Content-Type", "application/jason")
                 .body("{\n" +
                         "  \"location\": {\n" +
                         "    \"lat\": -38.383494,\n" +
@@ -26,13 +29,34 @@ public class Basics {
                         "  \"website\": \"http://google.com\",\n" +
                         "  \"language\": \"French-IN\"\n" +
                         "}\n")
-                            .when().post("/maps/api/place/add/json")
-                            .then().log().all().assertThat().statusCode(200).body("scope",equalTo("APP")).extract().asString()
-                                ;
+                .when().post("/maps/api/place/add/json")
+                .then().log().all().assertThat().statusCode(200).body("scope", equalTo("APP")).extract().asString();
 
-        JsonPath js=new JsonPath(response);//For parsing jason
-        String placeId=js.getString("place_id");
+        JsonPath js = ReUsableMethods.rawToJSon(response);//For parsing jason
+        String placeId = js.getString("place_id");
 
         System.out.println(placeId);
+
+
+//Updating the existing place address using placid
+        String newAddress = "70 Summer walk, USA";
+        given().config(RestAssured.config().encoderConfig(encoderConfig().encodeContentTypeAs("application/jason", ContentType.TEXT))).log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json").
+                body("{\n" +
+                        "\"place_id\":\"" + placeId + "\",\n" +
+                        "\"address\":\"" + newAddress + "\",\n" +
+                        "\"key\":\"qaclick123\"\n" +
+                        "}\n").
+                when().put("maps/api/place/update/json").
+                then().assertThat().log().all().statusCode(200).body("msg", equalTo("Address successfully updated"));
+
+//Retreive the updated place
+        String getPlaceResponse = given().config(RestAssured.config().encoderConfig(encoderConfig().encodeContentTypeAs("application/jason", ContentType.TEXT))).log().all().queryParam("key", "qaclick123").queryParam("place_id", placeId).
+                when().get("/maps/api/place/get/json").
+                then().assertThat().log().all().statusCode(200).extract().response().asString();
+
+        JsonPath js1 = ReUsableMethods.rawToJSon(getPlaceResponse);
+        String actualAddress = js1.getString("address");
+        System.out.println(actualAddress);
+        Assert.assertEquals(actualAddress, newAddress);
     }
 }
