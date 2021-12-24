f3dd045c2dcaa2774e47e708acd697144903df5b
ajinkyagh <ajinkyagh@gmail.com>
2021-12-24 08:51:08 +0530

    Api testing on Jira framework

---
+++ src/main/java/JiraTesting/JiraTest.java
@@ -0,0 +1,48 @@
+package JiraTesting;
+
+import io.restassured.RestAssured;
+import io.restassured.filter.session.SessionFilter;
+
+import java.io.File;
+
+import static io.restassured.RestAssured.given;
+
+public class JiraTest {
+    public static void main(String[] args){
+        RestAssured.baseURI="http://localhost:8080/";
+
+//        Login Scenario
+        SessionFilter session=new SessionFilter();//records output,// Reuse the same session filter instance to automatically apply the session id from the previous response
+
+        String response=given().header("Content-Type","application/json").body("{\n" +
+                "\t\"username\":\"ajinkyagh\",\n" +
+                "\t\"password\":\"Ajinkyagh@99#\"\n" +
+                "}").log().all().filter(session)
+                .when().post("/rest/auth/1/session")
+                .then().log().all().extract().response().asString();
+
+
+//Add comments
+        given().pathParam("key","10000").log().all().header("Content-Type","application/json").body("{\n" +
+                "    \"body\": \"second comment\",\n" +
+                "    \"visibility\": {\n" +
+                "        \"type\": \"role\",\n" +
+                "        \"value\": \"Administrators\"\n" +
+                "    }\n" +
+                "}").filter(session).when().post("/rest/api/2/issue/{key}/comment")
+                .then().log().all().assertThat().statusCode(201);
+
+        //    Add an attachment (Not working because jira free access has no attachments permission)
+       /* given().header("X-Atlassian-Token", "no-check").filter(session).pathParam("key","10000").header("Content-Type","multipart/form-data").multiPart("file",new File("jira.txt")).
+                when().post("/rest/api/2/issue/{key}/attachments").
+                then().log().all().assertThat().statusCode(200);*/
+
+//        Get issue
+        String issueDetails=given().filter(session).pathParam("key","10000").queryParam("fields","comment")
+                .when().get("/rest/api/2/issue/{key}")
+                .then().log().all().extract().response().asString();
+    }
+
+
+
+}
